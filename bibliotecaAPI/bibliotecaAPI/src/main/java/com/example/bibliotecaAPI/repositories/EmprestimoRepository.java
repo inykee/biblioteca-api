00437b5cecaef57ef162815e88978d3d5bd4a3d0
𝓲𝓷𝓎𝓴𝓮𝓮 <inykee.github@gmail.com>
2026-02-10 05:07:01 -0300

    Delete bibliotecaAPI directory

--- bibliotecaAPI/bibliotecaAPI/src/main/java/com/example/bibliotecaAPI/repositories/EmprestimoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bibliotecaAPI.repositories;
-
-import com.example.bibliotecaAPI.models.Emprestimo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Integer> {
-}
--- bibliotecaAPI/bibliotecaAPI/src/main/java/com/example/bibliotecaAPI/repositories/ExemplarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bibliotecaAPI.repositories;
-
-import com.example.bibliotecaAPI.models.Exemplar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExemplarRepository extends JpaRepository<Exemplar, Integer> {
-}
